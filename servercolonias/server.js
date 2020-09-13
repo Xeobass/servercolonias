ce1bc755a09d5ac468f71ca78a80eb6ec816eadf
Nelson Pedrosa <mazaroca_de_millo@hotmail.com>
2020-09-13 11:33:57 +0200

    Test desde app

---
+++ servercolonias/server.js
@@ -0,0 +1,24 @@
+const port = 9876;
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// parse requests of content-type: application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type: application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+
+
+require("./app/routes/volunteers.routes.js")(app);
+
+app.listen(port,()=>{
+    console.log(`Servidor escuchando en puerto ${port}`);
+});
