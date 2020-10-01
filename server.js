8278bbf3ec63e4aa6b94b5dc6e269055d32afada
Nelson Pedrosa Pena <xeobass@gmail.com>
2020-10-01 19:48:08 +0000

    agregada llamada a veterinarios

--- server.js
+++ server.js
@@ -2,14 +2,16 @@
 const port = 9876;
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require('cors')
 
 const app = express();
+app.use(cors());
 
 // parse requests of content-type: application/json
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '50mb', extended: true}));
 
 // parse requests of content-type: application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb',type:'application/x-www-form-urlencoded' }));
 
 // simple route
 app.get("/", (req, res) => {
@@ -18,7 +20,9 @@ app.get("/", (req, res) => {
 
 
 
+require("./app/routes/veterinarios.routes.js")(app);
 require("./app/routes/volunteers.routes.js")(app);
+require("./app/routes/gatos.routes.js")(app);
 
 app.listen(port,()=>{
     console.log(`Servidor escuchando en puerto ${port}`);
