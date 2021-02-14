4dc510df7fa55a61fa4269dd28afe9cd2cc01e2e
Nelson Pedrosa Pena <xeobass@gmail.com>
2021-02-14 21:04:44 +0100

    modificado datos

--- app/models/tests.model.js
+++ app/models/tests.model.js
@@ -2,25 +2,16 @@ const sql = require("./db.js");
 
 // constructor
 const Tests = function(test) {
-  this.idGato = gato.idGato;
-  this.idColonia = gato.idColonia;
-  this.idEsterilizacion = gato.idEsterilizacion;
-  this.idChip = gato.idChip;
-  this.idSituacion = gato.idSituacion;
-  this.idSexo = gato.idSexo;
-  this.idPositivo = gato.idPositivo;
-  this.nombreGato = gato.nombreGato;
-  this.imagenGato = gato.imagenGato;
-  this.peso = gato.peso;
-  this.marcaEsterilizacion = gato.marcaEsterilizacion;
-  this.caracteristicas = gato.caracteristicas;
-  this.nacimientoGato = gato.nacimientoGato;
-  this.fallecimientoGato = gato.fallecimientoGato;
+    idTest=req.body.idGato,
+    idClinicaVeterinaria = req.body.idColonia,
+    idResultadoTest = req.body.idEsterilizacion,
+    idGato = req.body.idChip,
+    fechaTest = req.body.idSituacion
   console.log("TESTS:",test);
 };
 
 
-Gatos.create = (newGato, result) => {
+Tests.create = (newGato, result) => {
     sql.query("INSERT INTO gatos SET ?", newGato, (err, res) => {
       if (err) {
         console.log("error: ", err);
@@ -35,7 +26,7 @@ Gatos.create = (newGato, result) => {
 
   
 
-  Gatos.findById = (volunteerId, result) => {
+  Tests.findById = (volunteerId, result) => {
     sql.query(`SELECT * FROM usuario WHERE id = ${volunteerId}`, (err, res) => {
       if (err) {
         console.log("error: ", err);
@@ -56,36 +47,9 @@ Gatos.create = (newGato, result) => {
 
   
 
-  Gatos.credentials = (volunteerPass,volunteerName,result)=>{
-    sql.query(`SELECT * FROM usuario WHERE usrnombre =\'${volunteerName}\' AND usrpass=\'${volunteerPass}\';`,(err,res)=>{
-      if(err){
-        console.log("Error solicitando datos de login:",err);
-        result(err,null);
-        return;
-      }
+  
 
-      if(res.length){
-        result(null,res[0]);
-        return;
-      }
-
-      result({kind:"not_found"},null);
-    })
-  }
-
-  Gatos.getColonias = result =>{
-    sql.query("SELECT * FROM `coloniasIdentificadas` ORDER BY `coloniasIdentificadas`.`nombreColonia` ASC",(err,res)=>{
-      if(err){
-        console.log("error: ",err);
-        result(null,err);
-        return;
-      }
-
-      result(null,res);
-    })
-  }
-
-  Gatos.getAll = result => {
+  Tests.getAll = result => {
     sql.query("SELECT * FROM gatos", (err, res) => {
       if (err) {
         console.log("error: ", err);
@@ -98,59 +62,9 @@ Gatos.create = (newGato, result) => {
     });
   };
 
-  Gatos.getSexo = result => {
-    sql.query("SELECT * FROM sexoGato", (err, res) => {
-      if (err) {
-        console.log("error: ", err);
-        result(null, err);
-        return;
-      }
   
-      console.log("sexo de gatos: ", res);
-      result(null, res);
-    });
-  };
-
-  Gatos.getSituacion = result => {
-    sql.query("SELECT * FROM situacionGato", (err, res) => {
-      if (err) {
-        console.log("error: ", err);
-        result(null, err);
-        return;
-      }
   
-      console.log("sexo de gatos: ", res);
-      result(null, res);
-    });
-  };
-
-  Gatos.getPositivo = result => {
-    sql.query("SELECT * FROM positivo", (err, res) => {
-      if (err) {
-        console.log("error: ", err);
-        result(null, err);
-        return;
-      }
-  
-      console.log("positivos: ", res);
-      result(null, res);
-    });
-  };
-
-  Gatos.getLastGato = result => {
-    sql.query("SELECT idGato,nombreGato FROM gatos ORDER BY idGato DESC LIMIT 1", (err, res) => {
-      if (err) {
-        console.log("error: ", err);
-        result(null, err);
-        return;
-      }
-  
-      console.log("datos último gato: ", res);
-      result(null, res);
-    });
-  };
-  
-  Gatos.updateById = (id, volunteer, result) => {
+  Tests.updateById = (id, volunteer, result) => {
     sql.query(
       "UPDATE usuario SET email = ?, name = ?, active = ? WHERE id = ?",
       [volunteer.email, volunteer.name, volunteer.active, id],
@@ -173,7 +87,7 @@ Gatos.create = (newGato, result) => {
     );
   };
   
-  Gatos.remove = (id, result) => {
+  Tests.remove = (id, result) => {
     sql.query("DELETE FROM usuario WHERE id = ?", id, (err, res) => {
       if (err) {
         console.log("error: ", err);
@@ -192,7 +106,7 @@ Gatos.create = (newGato, result) => {
     });
   };
   
-  Gatos.removeAll = result => {
+  Tests.removeAll = result => {
     sql.query("DELETE FROM usuario", (err, res) => {
       if (err) {
         console.log("error: ", err);
@@ -205,4 +119,4 @@ Gatos.create = (newGato, result) => {
     });
   };
   
-  module.exports = Gatos;
+  module.exports = Tests;
